95ccd9c9cb0fbacc856ef9e635c0310903299482
TYJ <tanyj-wm23@student.tarc.edu.my>
2024-08-26 00:59:39 +0800

    Checking sql database table

--- src/main/java/Entity/Purchase_Order.java
+++ src/main/java/Entity/Purchase_Order.java
@@ -6,10 +6,11 @@ import lombok.Data;
 @Builder
 public class Purchase_Order {
     private int po_number;
+    private int supplier_id;
     private int product_id;
     private int purchase_qty;
     private double order_price;
-    private int supplier_id;
+    private String order_date;
 
     public Purchase_Order(int po_number, int product_id, int purchase_qty, double order_price, int supplier_id) {
         this.po_number = po_number;
@@ -17,5 +18,14 @@ public class Purchase_Order {
         this.purchase_qty = purchase_qty;
         this.order_price = order_price;
         this.supplier_id = supplier_id;
+    }
+
+    public Purchase_Order(int po_number, int supplier_id, int product_id, double order_price, int purchase_qty, String order_date) {
+        this.po_number = po_number;
+        this.supplier_id = supplier_id;
+        this.product_id = product_id;
+        this.order_price = order_price;
+        this.purchase_qty = purchase_qty;
+        this.order_date = order_date;
     }
 }
